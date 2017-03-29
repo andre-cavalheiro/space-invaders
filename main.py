974f5b2986dc1d8846bfca912ba28f5830624a5f
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-03-29 00:28:11 +0000

    cores

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 """CORES:
-    1-nave espacial
-    2-inimigos
-    3-Tiros
+    1-nave espacial - 0 0 204
+    2-inimigos - 204 0 0
+    3-Tiros da nave 160 160 160 
 """
 from threading import Timer
+
+#Função que premite testar o funcionamento do programa
+def test(g):
+    for i in range(len(g.matrix)):
+        print(g.matrix[i])
+    print("----")
+    t = Timer(1,test,[g])
+    t.start()
 
 #Defenição de classes
 class spaceship:
@@ -24,20 +32,35 @@ class spaceship:
                 g.matrix[self.row][self.col[0]-1]=1
                 self.col = [x-1 for x in self.col]
 
-            g.print_me()
+
     def fire(self, g, row, col):
         #Apagar tiro anterior caso não seja o primeiro
         if row != 19:
             g.matrix[row][col]=0
 
+        #Se encontrar um alien matá-lo
+        if g.matrix[row-1][col] == 2:
+            print("let's kill it")
+            g.matrix[row-1][col] == 0
+            y = 0
+            x = 4
+            aux = g.enemy.pos[1]
+            while aux != col:
+                y += 1
+                aux += 2
+            while g.enemy.lives[x][y] != 1:
+                x-=1
+            g.enemy.lives[x][y]=0
+            return
+
         #Desenhar o novo tiro
         g.matrix[row-1][col]=3
 
-        g.print_me()
+        #Se chegarmos ao fim da matriz, o tiro desapareces
+        if row-1 == 0:
+            g.matrix[row-1][col]=0
+            return
 
-        #Se chegarmos ao fim da matriz, para o tiro
-        """if row-1 == 0:
-            return"""
         #Propagar o tiro:
         t = Timer(1/5, self.fire, [g, row-1, col])
         t.start()
@@ -58,6 +81,7 @@ class aliens:
             for j in range(4):
                     g.matrix[x][y]=0
                     y += 2
+        print("Apagado")
         #Alterar coordenadas
         if self.direction > 0:
             if self.pos[1] == 3:
@@ -72,6 +96,7 @@ class aliens:
             else:
                 self.pos[1] -=1
         #Escrever os Novos
+        print("Vms desenhar")
         x=self.pos[0]-2
         for i in range(5):
             x+=2
@@ -80,9 +105,9 @@ class aliens:
                     if self.lives[i][j] == 1:
                         g.matrix[x][y]=2
                     y += 2
-        g.print_me()
+        print("Desenhado")
         #Continuar a andar
-        t = Timer(3,self.move,[g])
+        t = Timer(1,self.move,[g])
         t.start()
 
     def fire(self):
@@ -113,13 +138,36 @@ class game:
         for i in range(len(self.matrix)):
             print(self.matrix[i])
         print("----")
+    
+    def check_win(self):
+         for i in range(5):
+            for j in range(4):
+                if self.lives[i][j] != 0:
+                    #Se existir algum inimigo vivo não acaba
+                    return 0    
+        #Caso contrário o jogo acaba
+        return 1
+    
+
+
 
 
 #Programa
 g = game()
-g.print_me()
-#g.enemy.move(g)
+test(g)
+
+"""
+g.enemy.move(g)
+
+
 g.ship.fire(g,g.ship.row,g.ship.col[1])
-print("Running")
 g.ship.move(g,1)
 g.ship.fire(g,g.ship.row,g.ship.col[1])
+
+g.ship.move(g,1)
+g.ship.fire(g,g.ship.row,g.ship.col[1])
+
+g.ship.move(g,-1)
+g.ship.move(g,-1)
+g.ship.move(g,-1)
+"""
