87d8d1bd5baa3fd901b161f686b42e782e9bf87f
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-04-01 23:50:01 +0000

    Adaptacoes para a matriz

--- space-invaders.py
+++ space-invaders.py
@@ -1,17 +1,8 @@
-"""CORES:
-    1-nave espacial - 0 0 204
-    2-inimigos - 204 0 0
-    3-Tiros da nave 160 160 160 
-"""
-from threading import Timer
+import socket
+import time 
+import threading 
+from queue import Queue
 
-#Função que premite testar o funcionamento do programa
-def test(g):
-    for i in range(len(g.matrix)):
-        print(g.matrix[i])
-    print("----")
-    t = Timer(1,test,[g])
-    t.start()
 
 #Defenição de classes
 class spaceship:
@@ -37,10 +28,8 @@ class spaceship:
         #Apagar tiro anterior caso não seja o primeiro
         if row != 19:
             g.matrix[row][col]=0
-
         #Se encontrar um alien matá-lo
         if g.matrix[row-1][col] == 2:
-            print("let's kill it")
             g.matrix[row-1][col] == 0
             y = 0
             x = 4
@@ -52,25 +41,23 @@ class spaceship:
                 x-=1
             g.enemy.lives[x][y]=0
             return
-
         #Desenhar o novo tiro
         g.matrix[row-1][col]=3
-
         #Se chegarmos ao fim da matriz, o tiro desapareces
         if row-1 == 0:
             g.matrix[row-1][col]=0
             return
-
         #Propagar o tiro:
         t = Timer(1/5, self.fire, [g, row-1, col])
         t.start()
 
-class aliens:
-    pos = [0,0]
-    direction = 1
 
+
+class aliens:
     def __init__(self):
         self.lives = [[1 for x in range(4)] for y in range(5)]
+        self.pos = [0,0]
+        self.direction = 1  #Quando positivo anda da esquerda para a direita
 
     def move(self, g):
         #Apagar Anteriores
@@ -107,18 +94,19 @@ class aliens:
         t = Timer(1,self.move,[g])
         t.start()
 
-    def fire(self):
-        return
 
 
 
 
-class game:
-    ship = spaceship()
-    enemy = aliens()
-    def __init__(self):
+class Space_invaders:
+    def __init__(self,matrix_s, input_queue):
+        self.socket = matrix_s
+        self.input_queue = input_queue
+        self.ship = spaceship()
+        self.enemy = aliens()
         #Iniciar matriz que simula a matriz de leds
-        self.matrix = [[0 for x in range(10)] for y in range(20)]
+        #self.matrix = [[0 for x in range(10)] for y in range(20)]
+        self.matrix = [[BLACK]*WIDTH for x in range(HEIGHT)]
         #Desenhar a nave espacial na sua posição inicial
         for n in self.ship.col:
             self.matrix[self.ship.row][n]=1
@@ -130,41 +118,46 @@ class game:
             for j in range(4):
                     self.matrix[x][y]=2
                     x +=2
+        self.loop()
 
-    def print_me(self):
-        for i in range(len(self.matrix)):
-            print(self.matrix[i])
-        print("----")
-    
     def check_win(self):
-         for i in range(5):
+        for i in range(5):
             for j in range(4):
                 if self.lives[i][j] != 0:
                     #Se existir algum inimigo vivo não acaba
-                    return 0    
+                    return 0
         #Caso contrário o jogo acaba
         return 1
+        
+    def update_screen(self):
+        screen = b''
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                screen += bytes(self.matrix[j][i])
+        
+        self.socket.sendall()
     
+    def loop(self):
+        while True:
+
+            if not self.input_queue.empty():
+                btn = self.input_queue.get()
+
+                if btn == 'Left':
+                    self.ship.move(self,-1)
+                elif btn == 'Right':
+                    self.ship.move(self,1)
+                elif btn == 'A':
+                    self.ship.fire(self,self.ship.row,self.ship.col[1])
+                
+                self.update_screen()
+                
+            
+                
 
 
 
 
-#Programa
-g = game()
-test(g)
-
-"""
-g.enemy.move(g)
 
 
-g.ship.fire(g,g.ship.row,g.ship.col[1])
-g.ship.move(g,1)
-g.ship.fire(g,g.ship.row,g.ship.col[1])
 
-g.ship.move(g,1)
-g.ship.fire(g,g.ship.row,g.ship.col[1])
-
-g.ship.move(g,-1)
-g.ship.move(g,-1)
-g.ship.move(g,-1)
-"""
